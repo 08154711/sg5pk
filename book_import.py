9cc9ada241061e35c1f75ee21acd5ede5178a521
robinarthur <stukka@gmx.de>
2017-10-20 08:21:07 +0200

    create book import.py

---
+++ book_import.py
@@ -0,0 +1,176 @@
+
+
+
+# The books are splitted into several files. First i have to group the 
+# files for every book and put them into one textstring
+
+# html_folder
+loc = "./html"
+
+#book1 - Demetrius
+raw_book_1_1 = loc + '/' + 'Demetrius.html'
+raw_book_1 = [raw_book_1_1]
+
+
+#book2 - Der versöhnte Menschenfeind
+raw_book_2_1 = loc + '/' + 'DerVers_0.html'
+raw_book_2_2 = loc + '/' + 'DerVers_1.html'
+raw_book_2 = [raw_book_2_1, raw_book_2_2]
+
+
+#book3 - Die Braut von Messina
+raw_book_3_1 = loc + '/' + 'DieBraut_0.html'
+raw_book_3_2 = loc + '/' + 'DieBraut_1.html'
+raw_book_3_3 = loc + '/' + 'DieBraut_2.html'
+raw_book_3_4 = loc + '/' + 'DieBraut_3.html'
+raw_book_3_5 = loc + '/' + 'DieBraut_4.html'
+raw_book_3_6 = loc + '/' + 'DieBraut_5.html'
+raw_book_3_7 = loc + '/' + 'DieBraut_6.html'
+raw_book_3 = [raw_book_3_1, raw_book_3_2, raw_book_3_3, raw_book_3_4, 
+              raw_book_3_5, raw_book_3_6, raw_book_3_7
+             ]
+
+
+#book4 - Die Huldigung der Künste - 1 - DieHuldi.html
+raw_book_4 = [loc + '/' + 'DieHuldi.html']
+
+
+#book5 - Die Jungfrau von Orleans - 10 - DieJungfrau_00.html
+raw_book_5_1 = loc + '/' + 'DieJungfrau_00.html'
+raw_book_5_2 = loc + '/' + 'DieJungfrau_01.html'
+raw_book_5_3 = loc + '/' + 'DieJungfrau_02.html'
+raw_book_5_4 = loc + '/' + 'DieJungfrau_03.html'
+raw_book_5_5 = loc + '/' + 'DieJungfrau_04.html'
+raw_book_5_6 = loc + '/' + 'DieJungfrau_05.html'
+raw_book_5_7 = loc + '/' + 'DieJungfrau_06.html'
+raw_book_5_8 = loc + '/' + 'DieJungfrau_07.html'
+raw_book_5_9 = loc + '/' + 'DieJungfrau_08.html'
+raw_book_5_10 = loc + '/' + 'DieJungfrau_09.html'
+raw_book_5 = [raw_book_5_1, raw_book_5_2, raw_book_5_3, raw_book_5_4,
+              raw_book_5_5, raw_book_5_6, raw_book_5_7, raw_book_5_8,
+              raw_book_5_9, raw_book_5_10]
+
+
+#book6 - Die Räuber - 9 - DieRauber_0.html
+raw_book_6_1 = loc + '/' + 'DieRauber_0.html'
+raw_book_6_2 = loc + '/' + 'DieRauber_1.html'
+raw_book_6_3 = loc + '/' + 'DieRauber_2.html'
+raw_book_6_4 = loc + '/' + 'DieRauber_3.html'
+raw_book_6_5 = loc + '/' + 'DieRauber_4.html'
+raw_book_6_6 = loc + '/' + 'DieRauber_5.html'
+raw_book_6_7 = loc + '/' + 'DieRauber_6.html'
+raw_book_6_8 = loc + '/' + 'DieRauber_7.html'
+raw_book_6_9 = loc + '/' + 'DieRauber_8.html'
+raw_book_6 = [raw_book_6_1, raw_book_6_2, raw_book_6_3, raw_book_6_4,
+              raw_book_6_5, raw_book_6_6, raw_book_6_7, raw_book_6_8,
+              raw_book_6_9]
+
+
+#book7 - Die Verschwörung des fiesco zu Genua 13 - DieVersch_00.html
+raw_book_7_1 = loc + '/' + 'DieVersch_00.html'
+raw_book_7_2 = loc + '/' + 'DieVersch_01.html'
+raw_book_7_3 = loc + '/' + 'DieVersch_02.html'
+raw_book_7_4 = loc + '/' + 'DieVersch_03.html'
+raw_book_7_5 = loc + '/' + 'DieVersch_04.html'
+raw_book_7_6 = loc + '/' + 'DieVersch_05.html'
+raw_book_7_7 = loc + '/' + 'DieVersch_06.html'
+raw_book_7_8 = loc + '/' + 'DieVersch_07.html'
+raw_book_7_9 = loc + '/' + 'DieVersch_08.html'
+raw_book_7_10 = loc + '/' + 'DieVersch_09.html'
+raw_book_7_11 = loc + '/' + 'DieVersch_10.html'
+raw_book_7_12 = loc + '/' + 'DieVersch_11.html'
+raw_book_7_13 = loc + '/' + 'DieVersch_12.html'
+raw_book_7 = [raw_book_7_1, raw_book_7_2, raw_book_7_3, raw_book_7_4,
+              raw_book_7_5, raw_book_7_6, raw_book_7_7, raw_book_7_8,
+              raw_book_7_9, raw_book_7_10, raw_book_7_11, raw_book_7_12,
+              raw_book_7_13]
+
+
+#book8 - Don Carlos - 10 - DonCarlos_0.html
+raw_book_8_1 = loc + '/' + 'DonCarlos_0.html'
+raw_book_8_2 = loc + '/' + 'DonCarlos_1.html'
+raw_book_8_3 = loc + '/' + 'DonCarlos_2.html'
+raw_book_8_4 = loc + '/' + 'DonCarlos_3.html'
+raw_book_8_5 = loc + '/' + 'DonCarlos_4.html'
+raw_book_8_6 = loc + '/' + 'DonCarlos_5.html'
+raw_book_8_7 = loc + '/' + 'DonCarlos_6.html'
+raw_book_8_8 = loc + '/' + 'DonCarlos_7.html'
+raw_book_8_9 = loc + '/' + 'DonCarlos_8.html'
+raw_book_8_10 = loc + '/' + 'DonCarlos_9.html'
+raw_book_8 = [raw_book_8_1, raw_book_8_2, raw_book_8_3, raw_book_8_4,
+              raw_book_8_5, raw_book_8_6, raw_book_8_7, raw_book_8_8,
+              raw_book_8_9]
+
+
+#book9 - Kabale und Liebe 7 - KabaleUnd_0.html
+raw_book_9_1 = loc + '/' + 'KabaleUnd_0.html'
+raw_book_9_2 = loc + '/' + 'KabaleUnd_1.html'
+raw_book_9_3 = loc + '/' + 'KabaleUnd_2.html'
+raw_book_9_4 = loc + '/' + 'KabaleUnd_3.html'
+raw_book_9_5 = loc + '/' + 'KabaleUnd_4.html'
+raw_book_9_6 = loc + '/' + 'KabaleUnd_5.html'
+raw_book_9_7 = loc + '/' + 'KabaleUnd_6.html'
+raw_book_9 = [raw_book_9_1, raw_book_9_2, raw_book_9_3, raw_book_9_4,
+              raw_book_9_5, raw_book_9_6, raw_book_9_7]
+
+
+#book10- Maria Stuart - 7 - MariaStuart_0.html
+raw_book_10_1 = loc + '/' + 'MariaStuart_0.html'
+raw_book_10_2 = loc + '/' + 'MariaStuart_1.html'
+raw_book_10_3 = loc + '/' + 'MariaStuart_2.html'
+raw_book_10_4 = loc + '/' + 'MariaStuart_3.html'
+raw_book_10_5 = loc + '/' + 'MariaStuart_4.html'
+raw_book_10_6 = loc + '/' + 'MariaStuart_5.html'
+raw_book_10_7 = loc + '/' + 'MariaStuart_6.html'
+raw_book_10 = [raw_book_10_1, raw_book_10_2, raw_book_10_3, raw_book_10_4,
+               raw_book_10_5, raw_book_10_6, raw_book_10_7]
+
+
+#book11- Semele
+raw_book_11_1 = loc + '/' + 'Semele_0.html'
+raw_book_11_2 = loc + '/' + 'Semele_1.html'
+raw_book_11_3 = loc + '/' + 'Semele_2.html'
+raw_book_11 = [raw_book_11_1, raw_book_11_2, raw_book_11_3]
+
+
+#book12- Wallenstein
+raw_book_12_1 = loc + '/' + 'Wallenstein_0.html'
+raw_book_12_2 = loc + '/' + 'Wallenstein_1.html'
+raw_book_12_3 = loc + '/' + 'Wallenstein_2.html'
+raw_book_12_4 = loc + '/' + 'Wallenstein_3.html'
+raw_book_12_5 = loc + '/' + 'Wallenstein_4.html'
+raw_book_12_6 = loc + '/' + 'Wallenstein_5.html'
+raw_book_12_7 = loc + '/' + 'Wallenstein_6.html'
+raw_book_12_8 = loc + '/' + 'Wallenstein_7.html'
+raw_book_12_9 = loc + '/' + 'Wallenstein_8.html'
+raw_book_12_10 = loc + '/' + 'Wallenstein_9.html'
+raw_book_12_11 = loc + '/' + 'Wallenstein_10.html'
+raw_book_12_12 = loc + '/' + 'Wallenstein_11.html'
+raw_book_12_13 = loc + '/' + 'Wallenstein_12.html'
+raw_book_12_14 = loc + '/' + 'Wallenstein_13.html'
+raw_book_12_15 = loc + '/' + 'Wallenstein_14.html'
+raw_book_12_16 = loc + '/' + 'Wallenstein_15.html'
+raw_book_12_17 = loc + '/' + 'Wallenstein_16.html'
+raw_book_12_18 = loc + '/' + 'Wallenstein_17.html'
+raw_book_12_19 = loc + '/' + 'Wallenstein_18.html'
+raw_book_12_20 = loc + '/' + 'Wallenstein_19.html'
+raw_book_12_21 = loc + '/' + 'Wallenstein_20.html'
+raw_book_12 = [raw_book_12_1, raw_book_12_2, raw_book_12_3, raw_book_12_4,
+               raw_book_12_5, raw_book_12_6, raw_book_12_7, raw_book_12_8,
+               raw_book_12_9, raw_book_12_10, raw_book_12_11, raw_book_12_12,
+               raw_book_12_13, raw_book_12_14, raw_book_12_15, raw_book_12_16,
+               raw_book_12_17, raw_book_12_18, raw_book_12_19, raw_book_12_20,
+               raw_book_12_21
+              ]
+
+#book13- Wilhelm Tell
+raw_book_13_1 = loc + '/' + 'WilhelmTell_0.html'
+raw_book_13_2 = loc + '/' + 'WilhelmTell_1.html'
+raw_book_13_3 = loc + '/' + 'WilhelmTell_3.html'
+raw_book_13_4 = loc + '/' + 'WilhelmTell_4.html'
+raw_book_13_5 = loc + '/' + 'WilhelmTell_5.html'
+raw_book_13_6 = loc + '/' + 'WilhelmTell_6.html'
+raw_book_13_7 = loc + '/' + 'WilhelmTell_7.html'
+raw_book_13 = [raw_book_13_1, raw_book_13_2, raw_book_13_3, raw_book_13_4,
+               raw_book_13_5, raw_book_13_6, raw_book_13_7
+              ]
